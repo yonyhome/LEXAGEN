c4dbfc541fa2e28f31c6f89c6edb22f2eab2df47
yonyhome <yonyh@uninorte.edu.co>
2025-04-22 14:38:12 -0500

    redirect

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,11 @@
-// vite.config.ts
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  build: {
-    outDir: 'dist', // 🔸 Firebase tomará este folder para servir el sitio
-    sourcemap: false,
-    emptyOutDir: true,
-  },
-  base: '/', // Muy importante para rutas relativas al deploy
-});
+  base: '/',  // o './' si usas subcarpeta
+  plugins: [
+    react(),
+    tsconfigPaths(),  // ← así Vite usará los paths de tu tsconfig
+  ],
+})
