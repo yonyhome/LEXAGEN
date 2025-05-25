515da432992f9decbab93ff47c2954f462dceb04
yonyhome <yonyh@uninorte.edu.co>
2025-05-25 14:32:43 -0500

    correct imports

--- src/components/home/FAQ.tsx
+++ src/components/home/FAQ.tsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { 
   ChevronDownIcon,
-  QuestionMarkCircleIcon,
   ClockIcon,
   CogIcon,
   CurrencyDollarIcon,
